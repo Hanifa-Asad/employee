c6149e31e09ddba0625952b879c79069c49ed6eb
Hanifa-Asad <hanifaasasd23@gmail.com>
2025-07-03 23:57:39 +0500

    frontend url added

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,19 @@ console.log("JWT_SECRET exists:", !!process.env.JWT_SECRET);
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "https://employee-teal.vercel.app",
+    ],
+    credentials: false,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 // MongoDB connection options
