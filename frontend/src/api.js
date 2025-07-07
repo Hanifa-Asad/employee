f0c9762c71d8f2d4e69307695a31c181200da50e
Hanifa-Asad <158709126+Hanifa-Asad@users.noreply.github.com>
2025-07-07 23:34:39 +0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://employee-utx8.vercel.app",
+  baseURL: "https://employee-utx8.vercel.app/api",
 
 });
 
