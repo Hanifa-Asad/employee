adb629bb9219e206283e000df6a1e37b87e77ae4
Hanifa-Asad <158709126+Hanifa-Asad@users.noreply.github.com>
2025-07-07 15:26:19 +0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,11 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-<<<<<<< HEAD
-  baseURL: "https://employee-3918.vercel.app",
-=======
   baseURL: "https://employee-teal.vercel.app",
->>>>>>> 2d1233aa39132459f872506025a70edf363720f6
+
 });
 
 // Attach token to every request if present
